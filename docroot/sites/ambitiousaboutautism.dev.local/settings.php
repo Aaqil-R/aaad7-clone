7942cb4a0da5938e36899a60063a6b5f3d4f6979
Acquia Cloud <support@acquia.com>
2014-12-22 07:33:40 +0000

    Importing site archive

--- docroot/sites/ambitiousaboutautism.dev.local/settings.php
+++ docroot/sites/ambitiousaboutautism.dev.local/settings.php
@@ -579,13 +579,5 @@ if (isset($_SERVER['DEVDESKTOP_DRUPAL_SETTINGS_DIR']) && file_exists($_SERVER['D
 // settings.php for development on your local workstation, set $db_url
 // (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
 if (file_exists('/var/www/site-php')) {
-}
-
-
-// On Acquia Cloud, this include file configures Drupal to use the correct
-// database in each site environment (Dev, Stage, or Prod). To use this
-// settings.php for development on your local workstation, set $db_url
-// (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
-if (file_exists('/var/www/site-php')) {
   require('/var/www/site-php/ambitiousaboutautism/ambitiousaboutautism-settings.inc');
 }
