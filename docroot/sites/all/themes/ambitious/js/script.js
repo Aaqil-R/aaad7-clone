a860d77b289ad667ccb88b2edf78e834a8988b85
kamalakannan <kamalakannan@globalsoftlab.com>
2015-01-21 11:56:16 +0530

    script added to fix masonory load more issue

--- docroot/sites/all/themes/ambitious/js/script.js
+++ docroot/sites/all/themes/ambitious/js/script.js
@@ -23,6 +23,18 @@ Drupal.behaviors.ambitious = {
 }
 };
 
+ $(window).bind('views_load_more.new_content', function(){
+    // Reload the masonry view after "load more"
+    if (typeOf Drupal.settings.masonry === 'object' && Drupal.settings.masonry != 'null'){
+      var view-ids = object.keys(Drupal.settings.masonry);
+      for(id in view-ids) {
+       if (myobj.hasOwnProperty(key)) {
+          $(id).masonry('reload');
+          console.log(id);
+       }
+      }
+    }
+  });
 
 })(jQuery, Drupal, this, this.document);
     //==Masonry block==//
