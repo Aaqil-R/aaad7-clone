87d8e5f34d93306ce92b51fcf560d74a345512cc
nanda <nanda@globalsoftlab.com>
2015-01-22 23:42:35 +0530

    share button issue fixed

--- docroot/sites/all/themes/ambitious/templates/views-exposed-form--stream.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-exposed-form--stream.tpl.php
@@ -21,7 +21,9 @@
  
  // var 
  
-  $node = node_load(arg(0));
+  $node = node_load(74461);
+  dpm(arg(0));
+    dpm(arg(1));
   $links = sharethis_node_view($node, 'full', 'en');
 ?>
 <?php if (!empty($q)): ?>
