cbb72e930ebf7d235d49f9af3c9a21751fbb43e0
nanda <nanda@globalsoftlab.com>
2015-01-26 20:15:28 +0530

    check before value set or not

--- docroot/sites/all/themes/ambitious/templates/views-view-masonry--stream--stream-topic-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-masonry--stream--stream-topic-page.tpl.php
@@ -9,11 +9,17 @@
   <?php if ($id == 0 && isset($variables['node'])):?> 
     <div class="masonry-item views-row views-row-1 views-row-odd views-row-first text-block post masonry-brick ">
       <!-- Teaser View of the topic --> 
-      <h1><?php print $variables['node']->title;?></h1>
-      <?php print $variables['node']->body['und'][0]['safe_value']; ?>
+      <?php if(isset($variables['node']->title)): ?>
+        <h1><?php print $variables['node']->title;?></h1>
+      <?php endif; ?>
+      <?php if(isset($variables['node']->body['und'][0]['safe_value'])): ?>
+        <?php print $variables['node']->body['und'][0]['safe_value']; ?>
+      <?php endif; ?>
       <div class="btn-holder clearfix">
         <a href="#" class="btn btn-gray btn-left" title="Filter by">Filter by</a>
-        <?php print render($variables['share_button']); ?> 
+        <?php if(isset($variables['share_button'])): ?>
+          <?php print render($variables['share_button']); ?> 
+        <?php endif; ?>
       </div>  
       <!-- Teaser ends here -->	
     </div>
