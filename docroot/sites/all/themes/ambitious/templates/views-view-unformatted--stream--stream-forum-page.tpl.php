fb14b3e21b270142005dd6b6976d15814d6d0b8c
nanda <nanda@globalsoftlab.com>
2015-01-28 22:07:17 +0530

    form page view content to feilds

---
+++ docroot/sites/all/themes/ambitious/templates/views-view-unformatted--stream--stream-forum-page.tpl.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row):?>
+<div class="clearfix">
+<?php if($id == 3){
+       $block = module_invoke('block', 'block_view', '96');
+	  print render($block['content']);
+     } ?>
+     </div>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+     <?php print $row;?>   
+  </div>
+<?php endforeach; ?>
