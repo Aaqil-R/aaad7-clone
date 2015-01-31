b4ec1fbae6a6d0146e7a2f502101ff560dec27dd
nanda <nanda@globalsoftlab.com>
2015-01-31 22:04:37 +0530

    feature code updated

--- docroot/sites/all/themes/ambitious/templates/views-view-masonry--stream-home-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-masonry--stream-home-page.tpl.php
@@ -7,9 +7,9 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<?php foreach ($rows as $id => $row): ?>
+<?php foreach ($rows as $id => $row): ?> 
 	<?php if ($id == 0): ?>
-		<div class="masonry-item<?php if ($classes_array[$id]) print ' ' . $classes_array[$id]; ?>">
+		<div class="feature-row masonry-item<?php if ($classes_array[$id]) print ' ' . $classes_array[$id]; ?>">
 		<?php
 		//D7
 		$block = module_invoke('block', 'block_view', '46');
@@ -17,7 +17,7 @@
 		?>
 		</div>
 	<?php endif; ?>
-  	<div class="masonry-item<?php if ($classes_array[$id]) print ' ' . $classes_array[$id]; ?>">
+  	<div class="<?php if ($id < 3){ print 'feature-row '; } ?>masonry-item<?php if ($classes_array[$id]) print ' ' . $classes_array[$id]; ?>">
     <?php print $row; ?>
   </div>
 <?php endforeach; ?>
