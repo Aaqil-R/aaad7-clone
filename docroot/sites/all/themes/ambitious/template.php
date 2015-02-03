2b3db1ee0e0609228cac9c584e84910e840cecb7
nanda <nanda@globalsoftlab.com>
2015-02-03 11:43:34 +0530

    form tool error fixed

--- docroot/sites/all/themes/ambitious/template.php
+++ docroot/sites/all/themes/ambitious/template.php
@@ -542,6 +542,12 @@ function ambitious_field__field_event_date(&$variables){
  return $output;	
 }
  
+ function ambitious_form_alter(&$form, &$form_state, $form_id)
+{
+  if($form_id == 'webform_client_form_74601'){
+    $form['submitted']['email_address']['#description'] = "<a class='tooltips'><span class='btn-tooltip'>?</span><span class='tooltip-content'>'.$form['submitted']['email_address']['#description'] .'</span></a>";
+  }
+}
 
 
 
