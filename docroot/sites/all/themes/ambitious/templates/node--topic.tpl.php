8d0ce63049528e3113a1acb973aa6ae0eb4db008
nanda <nanda@globalsoftlab.com>
2015-02-06 20:56:40 +0530

    breadcrumb topic page updated

--- docroot/sites/all/themes/ambitious/templates/node--topic.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/node--topic.tpl.php
@@ -48,9 +48,7 @@ $next_title = truncate_utf8($next_title,15, FALSE, TRUE, 3);
   <section class="visual">
     <div class="img-holder">
       <div class="caption-frame caption-frame-arrows"> 
-      <?php
-print theme('breadcrumb', drupal_get_breadcrumb());
-?>
+    
         <div class="caption">
           <?php if(isset($content['field_main_caption'])):?>
             <h1 class="title"><span><?php print render($content['field_main_caption']['#items'][0]['value']); ?></span></h1>
@@ -63,6 +61,9 @@ print theme('breadcrumb', drupal_get_breadcrumb());
       </div>
       <?php if(isset($content['field_featured_image'])):?>
 	    <img src="<?php print file_create_url($content['field_featured_image']['#items'][0]['uri']); ?>" alt="<?php print $title; ?>">
+	      <?php
+print theme('breadcrumb', drupal_get_breadcrumb());
+?>
 	  <?php endif; ?>
     </div>	 
     <?php if(isset($content['field_featured_image_by'])):?>
