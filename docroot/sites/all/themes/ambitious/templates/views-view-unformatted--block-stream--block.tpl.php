0a13e6989432fa831c2fc8f43efca4c86e70b17f
Nishan Mills <nishan@thesmallaxe.com>
2015-02-16 15:58:51 +0530

    Added a dpm to the row object.

--- docroot/sites/all/themes/ambitious/templates/views-view-unformatted--block-stream--block.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-unformatted--block-stream--block.tpl.php
@@ -6,19 +6,13 @@
  *
  * @ingroup views_templates
  */
-
 ?>
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<?php foreach ($rows as $id => $row):?>
-<div class="clearfix">
-<?php if($id == 3 && $view->query->pager->current_page === 0){
-       $block = module_invoke('block', 'block_view', '96');
-	  print render($block['content']);
-     } ?>
-     </div>
+<?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-     <?php print $row;?>   
+    <?php print $row; ?>
+    <?php dpm($row); ?>
   </div>
 <?php endforeach; ?>
