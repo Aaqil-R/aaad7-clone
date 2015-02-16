c1954adcff9fcd1f37c85e10775fdc462714ba62
nanda <nanda@globalsoftlab.com>
2015-02-16 14:56:07 +0530

    topic page color seection updated

--- docroot/sites/all/themes/ambitious/templates/views-view-fields--stream--stream-topic-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-fields--stream--stream-topic-page.tpl.php
@@ -1,11 +1,11 @@
 <?php
 $i = 1;
-$ourfields = array('title', 'body', 'field_has_video', 'field_featured_image', 'comment_count', 'type');
+$ourfields = array('title', 'body', 'field_has_video', 'field_featured_image', 'comment_count', 'type', 'field_background_colour', 'field_featured_author', 'field_source');
 ?>
  
 
 <?php if ($fields['type']->raw == 'quote'):?>
-<section class="blockquote-block" style="background:#<?php print $fields['field_background_colour']->raw; ?>" >
+<section class="blockquote-block" style="background:#<?php print $fields['field_background_colour']->content; ?>" >
 						<blockquote >
 							<q><?php print $fields['body']->content; ?></q>
 							<cite><?php print$fields['field_featured_author']->content; ?></cite>
@@ -34,7 +34,7 @@ $ourfields = array('title', 'body', 'field_has_video', 'field_featured_image', '
 						<div class="footer">
 							<div class="num-holder">
 								<a href="node/nid" title="people are talking about this" class="">
-									<span class="num"></span>
+									<span class="num"><?php print $fields['field_comment_count']->raw; ?></span>
 									<span class="text">people are talking about this </span>
 								</a>
 							</div>
