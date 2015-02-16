42013999ad4b4c78d03ef4d6c01a362f6ca8232b
nanda <nanda@globalsoftlab.com>
2015-02-16 19:03:29 +0530

    action block image created set bg - issue path

--- docroot/sites/all/themes/ambitious/templates/views-view-unformatted--block-stream--block.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-unformatted--block-stream--block.tpl.php
@@ -15,7 +15,7 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?> style="background-image: <?php print file_create_url(file_build_uri($myvar['variables']['view']->result[1]->field_field_background_image[0]['raw']['filename'])); ?>">
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?> style="background-image: url(<?php print file_create_url(file_build_uri($myvar['variables']['view']->result[1]->field_field_background_image[0]['raw']['filename'])); ?>)">
     <?php print $row; ?>   
   </div>
 <?php endforeach; ?>
