3bcc442ff9424a235853230319c849d4753df1d5
nanda <nanda@globalsoftlab.com>
2015-02-16 19:19:07 +0530

    action block set collum issue with fixed

--- docroot/sites/all/themes/ambitious/templates/views-view-unformatted--block-stream--block.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-unformatted--block-stream--block.tpl.php
@@ -16,15 +16,12 @@
 <?php endif; ?>
  
 <section class="action-block ">
-<div class="holder">
-<div class="region">
-<div class="block-main">
+ 
 <?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?> style="background-image: url(<?php print file_create_url(file_build_uri($myvar['variables']['view']->result[$id]->field_field_background_image[0]['raw']['filename'])); ?>)">
     <?php print $row; ?>   
   </div>
  
 <?php endforeach; ?>
- </div>
-   </div>
+  
 </section> 
