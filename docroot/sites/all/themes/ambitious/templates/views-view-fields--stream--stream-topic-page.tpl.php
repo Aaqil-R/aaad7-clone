35745a882dac99f0fc7dd42bf07caccfb1af3503
nanda <nanda@globalsoftlab.com>
2015-02-16 15:25:00 +0530

    topic page - Social Mention section updated - info error

--- docroot/sites/all/themes/ambitious/templates/views-view-fields--stream--stream-topic-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-fields--stream--stream-topic-page.tpl.php
@@ -28,7 +28,7 @@ $ourfieldsourse =  strip_tags($fields['field_source']->content);
 						<div class="info">
 							<h2><?php print $fields['title']->content; ?></h2>
 							<p>“<?php print strip_tags($fields['body']->content); ?>”</p>
-							<span class="cite"><strong>-</strong> <?php print$fields['field_featured_author']->content; ?></span>
+							<span class="cite"><strong>-</strong> <?php print strip_tags($fields['field_featured_author']->content); ?></span>
 						</div>
 <?php else:?>
 <section >
