9b00aff5be11c7e447d047c78cea80faf356712b
nanda <nanda@globalsoftlab.com>
2015-02-16 18:13:22 +0530

    action block backgroun created -error

--- docroot/sites/all/themes/ambitious/template.php
+++ docroot/sites/all/themes/ambitious/template.php
@@ -726,9 +726,10 @@ function ambitious_preprocess_views_view(&$vars) {
   if ($vars['view']->name == 'block_stream' || $vars['view']->vid) == '26'){
      $file_uri = file_create_url(file_build_uri('images/example.jpeg'));
      $backgroundurl = array();
-     foreach ( $vars['view']->result as $key => $value ){
+     foreach ($vars['view']->result as $key=>$value){
         $backgroundurl[$key] = $value;
+        dpm($backgroundurl);
      }
-     dpm($backgroundurl);
+
   }
 }
