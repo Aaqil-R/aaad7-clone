45535ae8ee6854f9abbde6094ee3b21bc980b7e6
nanda <nanda@globalsoftlab.com>
2015-02-16 14:17:22 +0530

    topic page quote section variable check - issues

--- docroot/sites/all/themes/ambitious/templates/views-view-fields--stream--stream-topic-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-fields--stream--stream-topic-page.tpl.php
@@ -1,11 +1,8 @@
 <?php
 $i = 1;
-$ourfields = array('title', 'body', 'field_has_video', 'field_featured_image', 'comment_count', 'type', 'field_background_colour','field_featured_author','field_source');
+$ourfields = array('title', 'body', 'field_has_video', 'field_featured_image',);
 ?>
-<?php print $fields['field_background_colour']; ?>
-<?php print $fields['field_featured_author']; ?>
-<?php print $fields['field_source']; ?> 
-<?php print $fields['comment_count']; ?>
+ 
 
 <?php if ($fields['type']->raw == 'quote'):?>
 <section class="blockquote-block">
