b0e4ba7cfd0ad55dcf1b8797ee052ec36629113d
nanda <nanda@globalsoftlab.com>
2015-02-17 21:05:21 +0530

    login page form tool tip added

--- docroot/sites/all/themes/ambitious/template.php
+++ docroot/sites/all/themes/ambitious/template.php
@@ -619,6 +619,10 @@ function ambitious_field__field_event_date(&$variables){
   } else if ($form_id == 'webform_client_form_74666') {
     $form['submitted']['email']['#description'] = "<a class='tooltips'><span class='btn-tooltip'>?</span><span class='tooltip-content'>".$form['submitted']['email']['#description']."</span></a>";
   }
+  else if ($form_id == 'user_login') {
+    $form['submitted']['name']['#description'] = "<a class='tooltips'><span class='btn-tooltip'>?</span><span class='tooltip-content'>".$form['submitted']['name']['#description']."</span></a>";
+    $form['submitted']['email']['#description'] = "<a class='tooltips'><span class='btn-tooltip'>?</span><span class='tooltip-content'>".$form['submitted']['pass']['#description']."</span></a>";
+  }
 }
 
 // Naming convention for .tpl.php
