f5062b99415fdfa9d52fef6fe8aee2eb5a5a200d
nanda <nanda@globalsoftlab.com>
2015-02-19 16:09:27 +0530

    events inner page remove extra > tag

--- docroot/sites/all/themes/ambitious/ds_layouts/event_col/event-col.tpl.php
+++ docroot/sites/all/themes/ambitious/ds_layouts/event_col/event-col.tpl.php
@@ -1,29 +1,8 @@
 <div class="<?php print $classes;?> clearfix">
   <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
-  <?php endif; ?>
-  
-  <?php if ($left): ?>
-    <div class="ds-left<?php print $left_classes; ?>">
-      <?php print $left; ?>
-    </div>
-  <?php endif; ?>
-
-
-  <?php if ($right): ?>
-    <div class="ds-right<?php print $right_classes; ?>">
-     
-    </div>
-  <?php endif; ?>
-  
-</div>
-
-
-<?php if (!empty($drupal_render_children)): ?>
-  <?php print $drupal_render_children ?>
-<?php endif; ?>
-
-<section class="my-voice-post">
+  <?php endif; ?> 
+  <section class="my-voice-post">
    <div>
      <?php print $image; ?>
      <a href="#" title="Featured" class="feature-holder">
@@ -37,7 +16,12 @@
      <div class="place mobile"><?php print $place; ?></div> 
      <?php print $content; ?>
    </div>
-   <div class="place desktop"><?php print $place; ?>></div> 
-</section> 
+   <div class="place desktop"><?php print $place; ?></div> 
+</section>  
+</div>
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
+
 
  
