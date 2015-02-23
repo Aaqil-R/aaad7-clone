e60026e08dd48f8cddfd9aa809fd6482bb2fe3f1
nanda <nanda@globalsoftlab.com>
2015-02-23 16:59:05 +0530

    header top bar profile image priented

--- docroot/sites/all/themes/ambitious/template.php
+++ docroot/sites/all/themes/ambitious/template.php
@@ -313,8 +313,13 @@ function ambitious_menu_link(&$variables) {
 			$file = file_load($fid);
 			// I found it necessary to use theme_image_style() instead of theme_user_picture()
 			// because I didn't want any extra html, just the image.
-			dpm($file);
-			$title = theme('image_style', array('style_name' => 'user_picture_thumb', 'path' => $file->uri, 'alt' => $element['#title'], 'title' => $element['#title'])) . $element['#title'];
+			if($fid == 0){
+			  $file = variable_get('user_picture_default'); 
+			}
+			// I found it necessary to use theme_image_style() instead of theme_user_picture()
+			// because I didn't want any extra html, just the image.			
+			$title = theme('image', array('path' => $file, 'alt' => $element['#title'], 'title' => $element['#title'])). $element['#title']; 
+			//$title = theme('image_style', array('style_name' => 'user_picture_thumb', 'path' => $file->uri, 'alt' => $element['#title'], 'title' => $element['#title'])) . $element['#title'];
 		}else {
 			$title = $element['#title'];
 		}
