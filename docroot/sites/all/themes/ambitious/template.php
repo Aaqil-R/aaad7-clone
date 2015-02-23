b032628a28175ec9d8338056e9a6ee1ca1c53dad
nanda <nanda@globalsoftlab.com>
2015-02-23 13:11:27 +0530

    forum page quote button fixed, bredgrume updated - error fixed

--- docroot/sites/all/themes/ambitious/template.php
+++ docroot/sites/all/themes/ambitious/template.php
@@ -764,6 +764,7 @@ function ambitious_preprocess_comment(&$vars){
   
   if(isset($vars['content']['links']['privatemsg'])){ 
      $vars['content']['links']['comment']['#links']['comment-reply'] = '';
+     $vars['content']['links']['privatemsg']['comment']['#links']['comment-reply'] = '';
      $vars['content']['links']['privatemsg']['#links']['privatemsg_link']['title'] = 'Contact Author';
      $vars['content']['privatemsg'] = $vars['content']['links'];
      $vars['content']['links']['privatemsg']['#links']['privatemsg_link'] = '';
