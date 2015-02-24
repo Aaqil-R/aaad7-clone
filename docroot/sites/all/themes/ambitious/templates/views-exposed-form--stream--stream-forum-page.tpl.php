bc0dadfbfed31c685eebd36c88a1b47f601e35ba
nanda <nanda@globalsoftlab.com>
2015-02-24 15:56:41 +0530

    forum page flags updated

--- docroot/sites/all/themes/ambitious/templates/views-exposed-form--stream--stream-forum-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-exposed-form--stream--stream-forum-page.tpl.php
@@ -29,7 +29,13 @@
   ?>
 <?php endif; ?> 
 
-
+<?php
+  $flag = flag_get_flag('bookmarks'); 
+  dpm($flag);
+  if (  $flag && $flag->is_flagged($node->nid)) {
+    print "This node is bookmarked!";
+ }
+?>
 
 <div class="sort-block">
 <div class="search-text forum-page"><input type="text" value="Search the community"></div>
