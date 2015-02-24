14bd3885a81a0ae9994b029a553fee248d3b7cd1
nanda <nanda@globalsoftlab.com>
2015-02-24 17:48:53 +0530

    topic page flag print in under text

--- docroot/sites/all/themes/ambitious/template.php
+++ docroot/sites/all/themes/ambitious/template.php
@@ -421,7 +421,7 @@ function ambitious_preprocess_views_view_masonry(&$vars) {
      $node = node_load(arg(1)); 
      $links = sharethis_node_view($node, 'full', 'en');
      $vars['share_button'] = '<div class="topic-share">'.$node->content['sharethis']['#value'].'</div>';
-     $vars['node'] = $node;
+     $vars['node'] = $node; 
   }
 
   if($vars['view']->current_display == 'understanding_autism_page' && $vars['view']->query->pager->current_page === 0){
--- docroot/sites/all/themes/ambitious/templates/views-view-masonry--stream--stream-topic-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-masonry--stream--stream-topic-page.tpl.php
@@ -11,13 +11,15 @@
       <!-- Teaser View of the topic --> 
       <?php if(isset($variables['node']->title)): ?>
         <h1><?php print $variables['node']->title;?></h1>
-      <?php endif; ?>
-       <?php $node = node_load(arg(1));
-         print flag_create_link('bookmarks', $node->nid);
-       ?>
+      <?php endif; ?> 
       <?php if(isset($variables['node']->body['und'][0]['safe_value'])): ?>
         <?php print $variables['node']->body['und'][0]['safe_value']; ?>
       <?php endif; ?>
+      <p>
+      <?php $node = node_load(arg(1));
+         print flag_create_link('bookmarks', $node->nid);
+       ?> 
+      </p>
          
       <!-- Teaser ends here -->	
     </div>
