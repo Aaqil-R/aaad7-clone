10c6189faee8f62f08ea2e1e79d9194f952dd481
kamalakannan <kamalakannan@globalsoftlab.com>
2015-02-28 15:55:02 +0530

    applied redirect to

--- docroot/sites/all/themes/ambitious/js/script.js
+++ docroot/sites/all/themes/ambitious/js/script.js
@@ -30,8 +30,7 @@ Drupal.behaviors.ambitious = {
 		   
 		   //navigate to donate page with the value from give what you can
 		   $("input[name='submitted[give_what_you_can]']").click(function () {
-		     var href = $("input[name='submitted[donate_page]']").val();
-		     console.log(href);
+         window.location = location.origin + $("input[name='submitted[donate_page]']").val() + '?amt='+$(this).val();
 		   });
 		
         // views filter opiton
