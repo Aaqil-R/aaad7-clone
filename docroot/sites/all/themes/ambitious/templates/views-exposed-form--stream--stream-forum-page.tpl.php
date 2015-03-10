21d12aabea66bccfe0521f60cc59d3ef55b2e1d5
nanda <nanda@globalsoftlab.com>
2015-03-10 19:23:43 +0530

    FORUM PAGE: Search - text focus issue on outer clink

--- docroot/sites/all/themes/ambitious/templates/views-exposed-form--stream--stream-forum-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-exposed-form--stream--stream-forum-page.tpl.php
@@ -32,8 +32,9 @@
 
 
 
+
 <div class="sort-block">
-<div class="search-text forum-page"><input type="text" value="Search the community" onfocus="if(this.value=='Search the community') this.value='';"><span class="icon-search"></span></div>
+<div class="search-text forum-page"><input type="text" value="Search the community" onfocus="javascript: if(this.value == 'Search the community'){ this.value = ''; }" onblur="javascript: if(this.value==''){this.value='Search the community';}" /><span class="icon-search"></span></div>
 				 <?php foreach ($widgets as $id => $widget): ?>
       <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
         <?php if (!empty($widget->label)): ?>
