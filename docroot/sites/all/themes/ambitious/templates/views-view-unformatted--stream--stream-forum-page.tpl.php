6c6818d877f3a5d9a874b22a079918557e281596
nanda <nanda@globalsoftlab.com>
2015-03-13 00:54:37 +0530

    star icons updated - error

--- docroot/sites/all/themes/ambitious/templates/views-view-unformatted--stream--stream-forum-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-unformatted--stream--stream-forum-page.tpl.php
@@ -12,18 +12,13 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row):?>
-<?php
-   $styleclass = '';
-   if($id < 3 && $view->query->pager->current_page === 0){
-        $styleclass = 'lessthen3';
-   } 
-   
-?> 
+<div class="clearfix">
 <?php /* if($id == 3 && $view->query->pager->current_page === 0){
        $block = module_invoke('block', 'block_view', '96');
 	  print render($block['content']);
-     }*/ ?> 
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] . $styleclass . '"';  } ?>>
+     }*/ ?>
+     </div>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
      <?php print $row;?>   
   </div>
 <?php endforeach; ?>
