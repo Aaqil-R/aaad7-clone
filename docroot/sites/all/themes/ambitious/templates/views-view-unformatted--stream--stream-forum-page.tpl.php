1a12f64911df2408596fb0e2b2a40a80ff79089d
nanda <nanda@globalsoftlab.com>
2015-03-13 01:13:18 +0530

    forum page tpl issue fixed

--- docroot/sites/all/themes/ambitious/templates/views-view-unformatted--stream--stream-forum-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-unformatted--stream--stream-forum-page.tpl.php
@@ -12,13 +12,18 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row):?>
-<div class="clearfix">
+<?php
+   $styleclass = '';
+   if($id < 3 && $view->query->pager->current_page === 0){
+        $styleclass = 'lessthen3';
+   } 
+   
+?> 
 <?php /* if($id == 3 && $view->query->pager->current_page === 0){
        $block = module_invoke('block', 'block_view', '96');
 	  print render($block['content']);
-     }*/ ?>
-     </div>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+     }*/ ?> 
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] . $styleclass . '"';  } ?>>
      <?php print $row;?>   
   </div>
 <?php endforeach; ?>
