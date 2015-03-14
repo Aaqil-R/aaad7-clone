3bce2f4f15f81e59317f577857f4e812b38c5f81
nanda <nanda@globalsoftlab.com>
2015-03-14 21:40:58 +0530

    action blocks added dpm

--- docroot/sites/all/themes/ambitious/templates/views-view-unformatted--block-stream--block.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-unformatted--block-stream--block.tpl.php
@@ -7,14 +7,14 @@
  * @ingroup views_templates
  */ 
  $myvar = get_defined_vars(); 
+ dpm($myvar);
 ?>
 
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?> 
 <?php foreach ($rows as $id => $row): ?>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?> style="background-image: url(<?php print file_create_url(file_build_uri($myvar['variables']['view']->result[$id]->field_field_background_image[0]['raw']['filename'])); ?>);background-size:cover;">
-   <?php dpm(($myvar['variables']['view']->result[$id]); ?>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?> style="background-image: url(<?php print file_create_url(file_build_uri($myvar['variables']['view']->result[$id]->field_field_background_image[0]['raw']['filename'])); ?>);background-size:cover;"> 
     <?php print $row; ?>   
   </div> 
 <?php endforeach; ?>
