0a007e893a469eefe9e6b9150a7c1de00941bf6b
nanda <nanda@globalsoftlab.com>
2015-03-16 22:57:03 +0530

    new button style updated, Topic Page - Infinite Scrolling. - fixed

--- docroot/sites/all/themes/ambitious/js/script.js
+++ docroot/sites/all/themes/ambitious/js/script.js
@@ -26,7 +26,7 @@ Drupal.behaviors.ambitious = {
 		 $.each(settings.viewsLoadMore, function(i, setting) {
 		   var view = '.view-id-' + setting.view_name + '.view-display-id-' + setting.view_display_id + ' .pager-next a';
 		   if (view == '.view-id-stream.view-display-id-stream_topic_page .pager-next a' && first == true){
-		    	$(window).load(function () {           
+		    	$(document).load(function () {           
 		     		$(view).waypoint('destroy');
 		   	});
 		   	first = false;
