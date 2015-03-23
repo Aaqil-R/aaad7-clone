51e594f0ad0b3987f3bec8322a8ce4f60de9c819
Nishan Mills <nishan@thesmallaxe.com>
2015-03-23 11:19:23 +0530

    Subtle style changes and code formatting.

--- docroot/sites/all/themes/ambitious/templates/page--basic-page-with-hero.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/page--basic-page-with-hero.tpl.php
@@ -121,11 +121,11 @@
               <?php endif; ?>
             </h1>
               <br>
-            <h3 class="banner_sub_text">
+            <h4 class="banner_sub_text">
               <?php if($captiontwo): ?>
                 <?php print render($captiontwo); ?> 
               <?php endif; ?>
-            </h3>          
+            </h4>          
             
         </div>
       </div>
@@ -271,27 +271,6 @@
       <div class="block">
         <div class="social-columns" >
           <?php print render($page['social']); ?>
-
-          <?php
-//Watch us youtube block
-          $block = module_invoke('block', 'block_view', '61');
-          print render($block['content']);
-          ?>
-
-          <!-- social column mid -->
-          <div class="col block-twitter">
-            <h3>Latest from Twitter</h3>
-            <div class="twitter">
-              <a class="twitter-timeline" href="https://twitter.com/AmbitiousAutism" data-widget-id="557083103072489472" width="300" height="500">Tweets by @AmbitiousAutism</a>
-              <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-            </div>
-          </div>
-          <!-- social column right -->
-          <div class="col block-facebook">
-            <h3>Find us on Facebook</h3>  
-            <div class="fb-like-box" data-href="https://www.facebook.com/ambitiousaboutautism" data-width="300" data-height="500" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="true" data-show-border="true"></div>
-
-          </div>
         </div>
       </div>
     </div>
--- docroot/sites/all/themes/ambitious/templates/page--page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/page--page.tpl.php
@@ -1,202 +1,194 @@
-                 <?php
-                 /**
-                  * @file
-                  * Bartik's theme implementation to display a single Drupal page.
-                  *
-                  * The doctype, html, head and body tags are not in this template. Instead they
-                  * can be found in the html.tpl.php template normally located in the
-                  * modules/system directory.
-                  *
-                  * Available variables:2
-                  *
-                  * General utility variables:
-                  * - $base_path: The base URL path of the Drupal installation. At the very
-                  *   least, this will always default to /.
-                  * - $directory: The directory the template is located in, e.g. modules/system
-                  *   or themes/bartik.
-                  * - $is_front: TRUE if the current page is the front page.
-                  * - $logged_in: TRUE if the user is registered and signed in.
-                  * - $is_admin: TRUE if the user has permission to access administration pages.
-                  *
-                  * Site identity:
-                  * - $front_page: The URL of the front page. Use this instead of $base_path,
-                  *   when linking to the front page. This includes the language domain or
-                  *   prefix.
-                  * - $logo: The path to the logo image, as defined in theme configuration.
-                  * - $site_name: The name of the site, empty when display has been disabled
-                  *   in theme settings.
-                  * - $site_slogan: The slogan of the site, empty when display has been disabled
-                  *   in theme settings.
-                  * - $hide_site_name: TRUE if the site name has been toggled off on the theme
-                  *   settings page. If hidden, the "element-invisible" class is added to make
-                  *   the site name visually hidden, but still accessible.
-                  * - $hide_site_slogan: TRUE if the site slogan has been toggled off on the
-                  *   theme settings page. If hidden, the "element-invisible" class is added to
-                  *   make the site slogan visually hidden, but still accessible.
-                  *
-                  * Navigation:
-                  * - $main_menu (array): An array containing the Main menu links for the
-                  *   site, if they have been configured.
-                  * - $secondary_menu (array): An array containing the Secondary menu links for
-                  *   the site, if they have been configured.
-                  * - $breadcrumb: The breadcrumb trail for the current page.
-                  *
-                  * Page content (in order of occurrence in the default page.tpl.php):
-                  * - $title_prefix (array): An array containing additional output populated by
-                  *   modules, intended to be displayed in front of the main title tag that
-                  *   appears in the template.
-                  * - $title: The page title, for use in the actual HTML content.
-                  * - $title_suffix (array): An array containing additional output populated by
-                  *   modules, intended to be displayed after the main title tag that appears in
-                  *   the template.
-                  * - $messages: HTML for status and error messages. Should be displayed
-                  *   prominently.
-                  * - $tabs (array): Tabs linking to any sub-pages beneath the current page
-                  *   (e.g., the view and edit tabs when displaying a node).
-                  * - $action_links (array): Actions local to the page, such as 'Add menu' on the
-                  *   menu administration interface.
-                  * - $feed_icons: A string of all feed icons for the current page.
-                  * - $node: The node object, if there is an automatically-loaded node
-                  *   associated with the page, and the node ID is the second argument
-                  *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
-                  *   comment/reply/12345).
-                  *
-                  * Regions:
-                  * - regions[header] = Header
-                  * - regions[highlighted] = Highlighted
-                  * - regions[content] = Content
-                  * - regions[content_bottom] = Content bottom
-                  * - regions[footer] = Footer
-                  *
-                  * @see template_preprocess()
-                  * @see template_preprocess_page()
-                  * @see template_process()
-                  * @see bartik_process_page()
-                  * @see html.tpl.php
-                  */
-                  //echo $stand_first;
-                  //hide($page['content']['field_stand_first']);
+<?php
+/**
+* @file
+* Bartik's theme implementation to display a single Drupal page.
+*
+* The doctype, html, head and body tags are not in this template. Instead they
+* can be found in the html.tpl.php template normally located in the
+* modules/system directory.
+*
+* Available variables:2
+*
+* General utility variables:
+* - $base_path: The base URL path of the Drupal installation. At the very
+*   least, this will always default to /.
+* - $directory: The directory the template is located in, e.g. modules/system
+*   or themes/bartik.
+* - $is_front: TRUE if the current page is the front page.
+* - $logged_in: TRUE if the user is registered and signed in.
+* - $is_admin: TRUE if the user has permission to access administration pages.
+*
+* Site identity:
+* - $front_page: The URL of the front page. Use this instead of $base_path,
+*   when linking to the front page. This includes the language domain or
+*   prefix.
+* - $logo: The path to the logo image, as defined in theme configuration.
+* - $site_name: The name of the site, empty when display has been disabled
+*   in theme settings.
+* - $site_slogan: The slogan of the site, empty when display has been disabled
+*   in theme settings.
+* - $hide_site_name: TRUE if the site name has been toggled off on the theme
+*   settings page. If hidden, the "element-invisible" class is added to make
+*   the site name visually hidden, but still accessible.
+* - $hide_site_slogan: TRUE if the site slogan has been toggled off on the
+*   theme settings page. If hidden, the "element-invisible" class is added to
+*   make the site slogan visually hidden, but still accessible.
+*
+* Navigation:
+* - $main_menu (array): An array containing the Main menu links for the
+*   site, if they have been configured.
+* - $secondary_menu (array): An array containing the Secondary menu links for
+*   the site, if they have been configured.
+* - $breadcrumb: The breadcrumb trail for the current page.
+*
+* Page content (in order of occurrence in the default page.tpl.php):
+* - $title_prefix (array): An array containing additional output populated by
+*   modules, intended to be displayed in front of the main title tag that
+*   appears in the template.
+* - $title: The page title, for use in the actual HTML content.
+* - $title_suffix (array): An array containing additional output populated by
+*   modules, intended to be displayed after the main title tag that appears in
+*   the template.
+* - $messages: HTML for status and error messages. Should be displayed
+*   prominently.
+* - $tabs (array): Tabs linking to any sub-pages beneath the current page
+*   (e.g., the view and edit tabs when displaying a node).
+* - $action_links (array): Actions local to the page, such as 'Add menu' on the
+*   menu administration interface.
+* - $feed_icons: A string of all feed icons for the current page.
+* - $node: The node object, if there is an automatically-loaded node
+*   associated with the page, and the node ID is the second argument
+*   in the page's path (e.g. node/12345 and node/12345/revisions, but not
+*   comment/reply/12345).
+*
+* Regions:
+* - regions[header] = Header
+* - regions[highlighted] = Highlighted
+* - regions[content] = Content
+* - regions[content_bottom] = Content bottom
+* - regions[footer] = Footer
+*
+* @see template_preprocess()
+* @see template_preprocess_page()
+* @see template_process()
+* @see bartik_process_page()
+* @see html.tpl.php
+*/
+//echo $stand_first;
+//hide($page['content']['field_stand_first']);
 
-                 /**
-                  * @file
-                  * Returns the HTML for a single Drupal page.
-                  *
-                  * Complete documentation for this file is available online.
-                  * @see https://drupal.org/node/1728148
-                  */
-                 ?>
+/**
+* @file
+* Returns the HTML for a single Drupal page.
+*
+* Complete documentation for this file is available online.
+* @see https://drupal.org/node/1728148
+*/
+?>
 
-           <div id="wrapper" class="page">
-             <a class="accessibility" href="#main" accesskey="s">Skip to Content</a>
+<div id="wrapper" class="page">
+  <a class="accessibility" href="#main" accesskey="s">Skip to Content</a>
 
-             <!-- including header region into the template -->
-           <?php
-             print render($page['header']); 
-           ?>
-         <!-- / header -->
-                 <?php if ($page['navigation']): ?>
-           <section id="navigation">
-             <?php print render($page['navigation']); ?>
-           </section> <!-- /navigation -->
-         <?php endif; ?>
+  <!-- including header region into the template -->
+  <?php
+    print render($page['header']); 
+  ?>
 
-         <?php if ($page['highlighted']): ?>
-           <section id="highlighted">
-             <?php print render($page['highlighted']); ?>
+  <!-- / header -->
+  <?php if ($page['navigation']): ?>
+    <section id="navigation">
+      <?php print render($page['navigation']); ?>
+    </section> <!-- /navigation -->
+  <?php endif; ?>
 
-             <?php if ($breadcrumb): ?>
-             <h1 class="breadcrumb"><?php print $breadcrumb; ?></h1>
-           <?php endif; ?>
+  <?php if ($page['highlighted']): ?>
+    <section id="highlighted">
+      <?php print render($page['highlighted']); ?>
 
-         </section> <!-- /highlighted -->
-         <?php endif; ?>
+        <?php if ($breadcrumb): ?>
+          <h1 class="breadcrumb"><?php print $breadcrumb; ?></h1>
+        <?php endif; ?>
 
-         <!-- top header of the page -->
-             <section class="top-header">
-               <div class="top-header-inner">
-                 <div class="page-links">
-                    <?php print render($page['content_top']); ?> 
-                 </div>
-               </div>
-             </section>
+    </section> <!-- /highlighted -->
+  <?php endif; ?>
 
-         <section class="header_banner banner-nav">
-          <!-- we must include the breadcrumb here and delete the below div -->
-         <div class="header_image">
-     <div class="region region-image-holder">
-           
-         </div>
-             </div>
+<!-- top header of the page -->
+  <section class="top-header">
+    <div class="top-header-inner">
+      <div class="page-links">
+        <?php print render($page['content_top']); ?> 
+      </div>
+    </div>
+  </section>
 
+  <section class="header_banner banner-nav">
+    <!-- we must include the breadcrumb here and delete the below div -->
+    <div class="header_image">
+      <div class="region region-image-holder">
+      </div>
+    </div>
+  </section>
 
-           </section>
-           <main id="main">
+  <main id="main">
+    <?php if ($title|| $messages || $tabs || $action_links): ?>
+      <div id="content-header">
+    <?php if ($title): ?>    
+      <h1 class="title"><?php print $title; ?></h1>
+    <?php endif; ?>
 
-
-             <?php if ($title|| $messages || $tabs || $action_links): ?>
-             <div id="content-header">
-
-               <?php if ($title): ?>
-               <h1 class="title"><?php print $title; ?></h1>
-             <?php endif; ?>
-
-
-             <?php print  $messages; ?>
-             <?php print render($page['help']); ?>
+    <?php print  $messages; ?>
+    <?php print render($page['help']); ?>
 
 
-             <?php if ($tabs): ?>
-             <div class="tabs"><?php print render($tabs); ?>
+    <?php if ($tabs): ?>
+      <div class="tabs"><?php print render($tabs); ?>
+      </div>
+    <?php endif; ?>
 
 
+    <?php if ($action_links): ?>
+     <ul class="action-links"><?php print render($action_links); ?></ul>
+    <?php endif; ?>
+
+    </div> <!-- /content-header -->
+    <?php endif; ?>
 
 
-             </div>
-           <?php endif; ?>
+    <section id="content-area">
+     <?php print render($page['content']) ?>
+    </section> <!-- /content -->
 
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
 
-           <?php if ($action_links): ?>
-             <ul class="action-links"><?php print render($action_links); ?></ul>
-           <?php endif; ?>
+    <?php if ($page['content_bottom']): ?>
+     <section id="content_bottom">
+       <?php print render($page['content_bottom']); ?>
+     </section> <!-- /content-bottom -->
+    <?php endif; ?>
 
-           </div> <!-- /content-header -->
-           <?php endif; ?>
+  </main> <!-- /main -->
 
+  <footer id="footer">
+   <div class="holder">
+     <div class="logo">
+       <a href="<?php print $front_page; ?>"><img alt="Ambitious About Autism" src="<?php print $base_path; ?>sites/all/themes/ambitious/images/logo-footer.png"></a>
+     </div>
+     <div class="right-footer">
+       <?php print render($page['footer_right']); ?>
+     </div><!-- /footer Right --> 
+     <div class="company-info">          
+       <?php print render($page['footer_copyright']); ?>
+       <span class="design-by">Designed and built by <a href="https://www.bluestatedigital.com" target="_blank">Blue State Digital</a>.</span>
+     </div><!-- /footer copyright -->              
+   </div>      
+  </footer> <!-- /footer -->
+  <a accesskey="t" href="#wrapper" class="accessibility">Back to top</a>  
 
-           <section id="content-area">
-             <?php print render($page['content']) ?>
-           </section> <!-- /content -->
+</div>
 
-           <?php
-                 // Render the sidebars to see if there's anything in them.
-           $sidebar_first  = render($page['sidebar_first']);
-           $sidebar_second = render($page['sidebar_second']);
-           ?>
-
-           <?php if ($page['content_bottom']): ?>
-             <section id="content_bottom">
-               <?php print render($page['content_bottom']); ?>
-             </section> <!-- /content-bottom -->
-           <?php endif; ?>
-
-           </main> <!-- /main -->
-
-           <footer id="footer">
-             <div class="holder">
-               <div class="logo">
-                 <a href="<?php print $front_page; ?>"><img alt="Ambitious About Autism" src="<?php print $base_path; ?>sites/all/themes/ambitious/images/logo-footer.png"></a>
-               </div>
-               <div class="right-footer">
-                 <?php print render($page['footer_right']); ?>
-               </div><!-- /footer Right --> 
-               <div class="company-info">          
-                 <?php print render($page['footer_copyright']); ?>
-                 <span class="design-by">Designed and built by <a href="https://www.bluestatedigital.com" target="_blank">Blue State Digital</a>.</span>
-               </div><!-- /footer copyright -->              
-             </div>      
-           </footer> <!-- /footer -->
-           <a accesskey="t" href="#wrapper" class="accessibility">Back to top</a>  
-
-           </div>
-           <?php print render($user_picture); ?>
-           <?php print render($page['bottom']); ?>
+<?php print render($user_picture); ?>
+<?php print render($page['bottom']); ?>
