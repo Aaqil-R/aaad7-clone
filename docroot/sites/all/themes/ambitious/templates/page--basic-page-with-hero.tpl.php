5e6fc5f24ff18687f5d0623ed1cc7850b0c5a0e7
Nishan Mills <nishan@thesmallaxe.com>
2015-03-23 07:36:42 +0530

    Correcting syntax errors on if statements.

--- docroot/sites/all/themes/ambitious/templates/page--basic-page-with-hero.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/page--basic-page-with-hero.tpl.php
@@ -116,13 +116,13 @@
           </div>
 
             <h1 class="banner_text"> 
-              <?php if($captionone) ?>
+              <?php if($captionone): ?>
                 <?php print render($captionone); ?>
               <?php endif; ?>
             </h1>
               <br>
             <h3 class="banner_sub_text">
-              <?php if($captiontwo) ?>
+              <?php if($captiontwo): ?>
                 <?php print render($captiontwo); ?> 
               <?php endif; ?>
             </h3>          
@@ -131,7 +131,7 @@
       </div>
       <div class="region region-image-holder">
         <p>
-          <?php if($image) ?>
+          <?php if($image): ?>
             <?php print render($image); ?>  
           <?php endif; ?>  
         </p>    
@@ -139,7 +139,7 @@
     </div>
     <div class="holder">
       <span class="pic-by">
-        <?php if($credit) ?>
+        <?php if($credit): ?>
           <?php print render($credit); ?> 
         <?php endif; ?> 
       </span>
