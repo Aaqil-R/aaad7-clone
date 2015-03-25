40a0a4dccc42c47ec33d6a61f3ac331b70fb5a22
Noah Amalan Premkumar <amalanpremkumar@gmail.com>
2015-03-25 18:55:02 +0530

    My voice link not working issue fixed.

--- docroot/sites/all/themes/ambitious/template.php
+++ docroot/sites/all/themes/ambitious/template.php
@@ -149,12 +149,13 @@ function ambitious_preprocess_region(&$variables) {
   $variables['myblock'] = drupal_render($block2);
   
   $variables['logo'] = theme_get_setting('logo'); 
+  $variables['link'] = "/";
   $variables['headerlogo'] = null;
   // TODO: This should become redundant as soon as we move to a different site.
   if ($currentNode) {
     if($currentNode->nid == 74596 || $currentNode->type == "my_voice_blog") { 
       $variables['logo'] = "/sites/all/themes/ambitious/images/my-voice-logo.png"; 
-
+      $variables['link'] = "/voices-spectrum";
       $block = block_load('block',151);
       $block1 = _block_render_blocks(array($block));
       $block2 = _block_get_renderable_array($block1);
