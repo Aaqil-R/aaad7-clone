fa97d7bc42abd3206140ce7b2b9734c57487ef38
kamalakannan <kamalakannan@globalsoftlab.com>
2015-03-25 18:33:10 +0530

    adjusted code to handle less items in understanding autism age page

--- docroot/sites/all/themes/ambitious/templates/views-view-masonry--understanding-autism-page-age.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-masonry--understanding-autism-page-age.tpl.php
@@ -5,7 +5,12 @@
  */
 
 // Variable
-$last=count($rows) - 1;
+if ($view->query->pager->total_items > $view->query->pager->options['items_per_page']){
+  $no=(int) ($view->query->pager->total_items/$view->query->pager->options['items_per_page']);
+  $noofpage = round($no,0, PHP_ROUND_HALF_DOWN);
+} else {
+ $noofpage = 0;
+}
 
 ?>
 
@@ -32,7 +37,9 @@ $last=count($rows) - 1;
 			<?php print $row; ?>
 		</div>
 
-	<?php if($id == $last): ?>
+
+<?php endforeach; ?>
+	<?php if($view->query->pager->current_page == $noofpage): ?>
 		<div class="masonry-item views-row views-row-even views-row-last post-my-voice-actions post masonry-brick">
 			<?php
 			//D7
@@ -43,7 +50,3 @@ $last=count($rows) - 1;
 			?>
 		</div>
 	<?php endif; ?>
-
-
-
-<?php endforeach; ?>
