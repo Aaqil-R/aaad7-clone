ce2260e6f3fa7e2ea22a3fc6923394233fa5dcdb
nanda <nanda@globalsoftlab.com>
2015-04-21 22:17:01 +0530

    views_embed_view added

--- docroot/sites/all/themes/ambitious/templates/node--forum_discussion.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/node--forum_discussion.tpl.php
@@ -126,8 +126,7 @@
   
  </div>
   </div>
-  
-  <?php  print views_embed_view('comments','comments'); ?>
+  <?php print views_embed_view('comments','comments', $node->nid); ?>
  			<?php if($content['comments']['comment_form']):?>   
 		<section class="comment-block">			
  <?php print render($content['comments']['comment_form']);   ?>
