77e4866fcb7cf62dc60fe9c9c035cc24399d6b69
nanda <nanda@globalsoftlab.com>
2015-04-23 14:51:33 +0530

    event page bsd url in image updated

--- docroot/sites/all/themes/ambitious/ds_layouts/event_col/event-col.tpl.php
+++ docroot/sites/all/themes/ambitious/ds_layouts/event_col/event-col.tpl.php
@@ -1,12 +1,22 @@
+<?php
+  $node = node_load(arg(1));
+  $bsd_url = $node->field_bsd_tools_integration['und'][0]['safe_value']; 
+?>
 <div class="<?php print $classes;?> clearfix">
   <?php if (isset($title_suffix['contextual_links'])): ?>
   <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?> 
   <section class="my-voice-post">
    <div>
-   <?php if ($image): ?>
-     <?php print $image; ?>
-       <?php endif; ?>
+    <?php if ($image): ?>
+     <?php if($bsd_url == ""):?>
+       <?php print $image; ?>
+     <?php else:?>
+     <a href="<?php print $bsd_url ?>" title="<?php print $node->title; ?>">
+       <?php print $image; ?>
+     </a>
+      <?php endif; ?>
+     <?php endif; ?>
      <a href="#" title="Featured" class="feature-holder">
        <span class="icon-Featured"></span>
        <span class="text">Featured</span>
