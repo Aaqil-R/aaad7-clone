5c26d089f5068af897ae0c4ea9ea4e879323fbd2
nanda <nanda@globalsoftlab.com>
2015-04-24 18:20:57 +0530

    added links to back fourm -2

--- docroot/sites/all/themes/ambitious/templates/node--forum_discussion.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/node--forum_discussion.tpl.php
@@ -131,7 +131,7 @@
  </div>
   </div>
   <?php print views_embed_view('comments','comments', $node->nid); ?>
-  <a href="/talk-to-others" title="Back to discussions">Back to discussions</a>
+  <p><a href="/talk-to-others" title="Back to discussions">Back to discussions</a></p>
  			<?php if($content['comments']['comment_form']):?>   
 		<section class="comment-block">			
  <?php print render($content['comments']['comment_form']);   ?>
