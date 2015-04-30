236259764d882d03b57d776189d8d7e227bc949d
Samantha Amarathunga <samantha@thesmallaxe.com>
2015-04-30 16:17:39 +0530

    Added back to profile link

--- docroot/sites/all/themes/ambitious/templates/privatemsg-recipients.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/privatemsg-recipients.tpl.php
@@ -2,7 +2,7 @@
   //each file loads it's own styles because we cant predict which file will be loaded 
   drupal_add_css(drupal_get_path('module', 'privatemsg').'/styles/privatemsg-recipients.css');
 ?>
+<div class="only-mobile margin-bottom-10"><a href="/user">Back to profile page</a></div>
 <div class="privatemsg-message-participants">
-<div class="only-mobile"><a href="/user">Back to profile</a></div>
   <?php print $participants; ?>
 </div>
