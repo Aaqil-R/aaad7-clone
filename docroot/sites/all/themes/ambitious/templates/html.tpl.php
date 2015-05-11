556a55bb60bddfacf068bc80229172b600e88a9c
Nanda Kumar <nanda@globalsoftlab.com>
2015-05-11 13:18:48 +0530

    added script for face book share button

--- docroot/sites/all/themes/ambitious/templates/html.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/html.tpl.php
@@ -55,5 +55,14 @@
     <?php print $page_top; ?>
     <?php print $page; ?>  
     <?php print $page_bottom; ?>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
   </body>
 </html>
