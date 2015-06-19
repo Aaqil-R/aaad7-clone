cd9d1eb6cc64af6ea80d854d44fbe121e1f16e80
Amalan Premkumar <amalanpremkumar@gmail.com>
2015-06-19 12:50:59 +0530

    indenting the code.

--- docroot/sites/all/themes/ambitious/templates/node--topic.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/node--topic.tpl.php
@@ -8,12 +8,12 @@
  */
 
 /* variables */ 
-  
-  $node = node_load(arg(1)); 
-  $links = sharethis_node_view($node, 'full', 'en');
+
+$node = node_load(arg(1)); 
+$links = sharethis_node_view($node, 'full', 'en');
 ?>  
 <?php if($view_mode == 'posts_columns'): ?>
-<!-- Teaser View of the topic -->
+  <!-- Teaser View of the topic -->
   <section class="text-block">
     <h1><?php print $title; ?></h1>
     <?php print render($content['body']);?>
@@ -22,10 +22,9 @@
       <div class="topic-share"><?php print $node->content['sharethis']['#value']; ?></div>
     </div>
   </section>	 
-    <!-- Teaser ends here -->				
+  <!-- Teaser ends here -->				
 <?php else: ?>
-
-<?php
+  <?php
   // Get the previous node id
   $prev_nid = prev_next_nid($nid, 'prev');
   // Get the previous node id
@@ -40,72 +39,65 @@
     // Get the previous node title
     $next_title = node_load($next_nid);    
     $next_title = $next_title->title;    
-  $next_title = truncate_utf8($next_title,15, FALSE, TRUE, 3);
+    $next_title = truncate_utf8($next_title,15, FALSE, TRUE, 3);
   } 
-?>
-<?php if($page):?>
+  ?>
+  <?php if($page):?>
   <section class="visual header_banner">
-  <div class="img-holder">
+    <div class="img-holder">
      <div class="banner_text_main">
       <div class="banner_nav visual">
-          <?php 
-            if (0 < $prev_nid){
-              print l('<span class="icon-Leftarrow"></span><div class="text-area"><span class="title">Previous topic:</span>          <span class="topic">'.$prev_title.'</span></div>','node/'.$prev_nid.'', array('html' => TRUE, 'attributes' => array('class' => 'btn-perv')));
-       }
+        <?php 
+        if (0 < $prev_nid){
+          print l('<span class="icon-Leftarrow"></span><div class="text-area"><span class="title">Previous topic:</span>          <span class="topic">'.$prev_title.'</span></div>','node/'.$prev_nid.'', array('html' => TRUE, 'attributes' => array('class' => 'btn-perv')));
+        }
         ?>  
-
         <?php
         if (0 < $next_nid){
          print l('<div class="text-area"><span class="title">Next topic:</span><span class="topic">'.$next_title.'</span></div>        <span class="icon-Rightarrow"></span>','node/'.$next_nid.'', array('html' => TRUE, 'attributes' => array('class' => 'btn-next'))); 
-        }
-        ?>
-        </div>
-    </div>
-    <div class="caption-frame">
-        <div class="region region-caption-holder">
-            
-            
-        <?php if ($page['breadcrumb']): ?>
-            <div class="easy_breadcrumb">
-              <?php print render($page['breadcrumb']); ?>
-            </div>
-          <?php endif; ?>  
-
-      
-           
-        <div class="caption-text">
-        <h1 class="caption-text-titles">
-            <?php if(isset($content['field_main_caption'])):?>
-            <div class="caption-text-title caption-text-title-1">               
-            <span>
-                <?php print render($content['field_main_caption']['#items'][0]['value']); ?>
-            </span>
-            </div>
-            <?php endif; ?>
-            <?php if(isset($content['field_sub_caption'])):?>
-                <div class="caption-text-title caption-text-title-2">               
-                    <span>
-                        <?php print render($content['field_sub_caption']['#items'][0]['value']); ?>
-                    </span>
-                </div>
-            <?php endif; ?> 
-        </h1>
-        </div>
-        </div>
+       }
+       ?>
+     </div>
+   </div>
+   <div class="caption-frame">
+    <div class="region region-caption-holder">
+      <?php if ($page['breadcrumb']): ?>
+      <div class="easy_breadcrumb">
+        <?php print render($page['breadcrumb']); ?>
       </div>
-    
-          <div class="region region-image-holder"
-               style="background-image: url('<?php print image_style_url('banner_1080',$content['field_featured_image']['#items'][0]['uri']); ?>')">
-          </div>
-          </div>
-    <?php if(isset($content['field_featured_image_by'])):?>
-    <div class="holder">
-          <span class="pic-by">         
-              <?php print t('© Photo by'); ?> 
-      <?php print render($content['field_featured_image_by']['#items'][0]['value']); ?>
+    <?php endif; ?>  
+    <div class="caption-text">
+      <h1 class="caption-text-titles">
+        <?php if(isset($content['field_main_caption'])):?>
+        <div class="caption-text-title caption-text-title-1">               
+          <span>
+            <?php print render($content['field_main_caption']['#items'][0]['value']); ?>
+          </span>
+        </div>
+      <?php endif; ?>
+      <?php if(isset($content['field_sub_caption'])):?>
+      <div class="caption-text-title caption-text-title-2">               
+        <span>
+          <?php print render($content['field_sub_caption']['#items'][0]['value']); ?>
         </span>
-    </div>
-   <?php endif; ?> 
-  </section>  
+      </div>
+    <?php endif; ?> 
+  </h1>
+</div>
+</div>
+</div>
+<div class="region region-image-holder"
+style="background-image: url('<?php print image_style_url('banner_1080',$content['field_featured_image']['#items'][0]['uri']); ?>')">
+</div>
+</div>
+<?php if(isset($content['field_featured_image_by'])):?>
+  <div class="holder">
+    <span class="pic-by">         
+      <?php print t('© Photo by'); ?> 
+      <?php print render($content['field_featured_image_by']['#items'][0]['value']); ?>
+    </span>
+  </div>
+<?php endif; ?> 
+</section>  
 <?php endif; ?>
-<?php endif; ?>   
+<?php endif; ?>
