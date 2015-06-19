af254c533b5e446b01809a75a4c6c85e23ab1dd7
Aaqil <aaqil@thesmallaxe.com>
2015-06-19 14:21:13 +0530

    Stay ambitious FB section issue on - iPad (Redo)

--- docroot/sites/all/themes/ambitious/templates/page--front.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/page--front.tpl.php
@@ -263,9 +263,10 @@
           </div>
           <!-- social column right -->
           <div class="col block-facebook">
-            <h3>Find us on Facebook</h3>  
-<div class="fb-like-box" data-href="https://www.facebook.com/ambitiousaboutautism" data-width="300" data-height="500" data-colorscheme="light" data-show-faces="true" data-header="false" data-stream="true" data-show-border="true"></div>
- 
+            <h3>Find us on Facebook</h3>
+              <div class="facebook">
+                  <div class="fb-like-box" data-href="https://www.facebook.com/ambitiousaboutautism" data-width="100" data-height="500" data-                       colorscheme="light" data-show-faces="true" data-header="false" data-stream="true" data-show-border="true"></div>
+                </div>
           </div>
         </div>
       </div>
