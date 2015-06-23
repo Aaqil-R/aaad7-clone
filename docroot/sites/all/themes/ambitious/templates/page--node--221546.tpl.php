0eb88a8eff365d0ba3758b1a8f71ac91e178df35
Nishan Mills <nishan@thesmallaxe.com>
2015-06-23 15:30:42 +0530

    Removed the fb-root implementation as it is already provided through html.tpl.php; if not it is duplicating on the html markup.

--- docroot/sites/all/themes/ambitious/templates/page--node--221546.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/page--node--221546.tpl.php
@@ -85,16 +85,6 @@
 * @see https://drupal.org/node/1728148
 */
 ?>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=1513349745593631&version=v2.0";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<!-- /facebook -->
-
 <!-- UNDERSTANDING AUTISM PAGE TEMPLATE -->
 <div id="wrapper" class="page">
   <a class="accessibility" href="#main" accesskey="s">Skip to Content</a>
