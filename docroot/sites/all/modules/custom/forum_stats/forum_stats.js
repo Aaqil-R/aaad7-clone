c9f5fa63035b67cd12cff20a7cf4cd219495b389
Nanda Kumar <nanda@globalsoftlab.com>
2015-07-09 20:48:02 +0530

    forum stats update front end filter

---
+++ docroot/sites/all/modules/custom/forum_stats/forum_stats.js
@@ -0,0 +1,73 @@
+(function ($) {
+  Drupal.behaviors.forumstats = {
+	    attach: function(context) { 
+	    $( document ).ready(function() {
+	    	$('#forum-stats-settings-form #edit-submit').hide();
+	      $('#forum-stats-settings-form .calendars').hide();
+	      $( "#selected_date_form" ).datepicker({
+          numberOfMonths:1, 
+          dateFormat : "d-M-yy",
+          onSelect: function( selectedDate ) {
+            console.log(selectedDate);
+            $( "#selected_date_to" ).datepicker( "option", "minDate", selectedDate );
+          }
+        });
+        $( "#selected_date_to" ).datepicker({
+          numberOfMonths:1, 
+          dateFormat : "d-M-yy",
+          onSelect: function( selectedDate ) {
+            console.log(selectedDate);          
+            $( "#selected_date_form" ).datepicker( "option", "maxDate", selectedDate );
+          }
+        }); 
+	       if (location.search != '') {
+	         selected = location.search.split('&');console.log(selected.length + 'l');
+	         if (selected.length > 0) {
+	         if (selected[0].indexOf('?page') != -1) {
+               selected.shift();
+           }
+           console.log(selected +  ' ' + selected[1]);
+	         selectopt = selected[0].split('=')[1];console.log(selectopt);
+	         } else {
+	         selectopt = location.search.split('=')[1];
+	         }
+	         if (selectopt == 4) {
+	           	$('#forum-stats-settings-form #edit-submit').show();
+	            $('#forum-stats-settings-form .calendars').show(); 
+	            $( "#selected_date_form" ).datepicker( "setDate", decodeURI(selected[1].split('=')[1]) );
+	            $( "#selected_date_to" ).datepicker( "setDate", decodeURI(selected[2].split('=')[1]) );
+	            $( "#selected_date_to" ).datepicker( "option", "minDate",  decodeURI(selected[1].split('=')[1]));
+	            $( "#selected_date_form" ).datepicker( "option", "maxDate", decodeURI(selected[2].split('=')[1]) ); 
+	            $('#forum-stats-settings-form').addClass('custom_option');
+	         }
+	         else{
+	            $('#forum-stats-settings-form').removeClass('custom_option');
+	         }
+	       }
+
+         $('#forum-stats-settings-form #edit-selected').change(function () {
+         if($(this).val() != 4){
+           $('#forum-stats-settings-form #edit-submit').hide();
+	         $('#forum-stats-settings-form .calendars').hide();           
+           location.href = location.origin + location.pathname + '?selected=' + $(this).val();
+            $('#forum-stats-settings-form').removeClass('custom_option');
+         } else {
+	        $('#forum-stats-settings-form').addClass('custom_option');
+          $('#forum-stats-settings-form #edit-submit').show();
+	        $('#forum-stats-settings-form .calendars').show();           
+         }
+       });
+    
+        $('#forum-stats-settings-form').submit(function (e) {
+        e.preventDefault();
+        from = $.datepicker.formatDate( "d-M-yy", $( "#selected_date_form" ).datepicker('getDate') );
+        to = $.datepicker.formatDate( "d-M-yy", $( "#selected_date_to" ).datepicker('getDate') );
+        location.href = location.origin + location.pathname + '?selected=4&from=' + from + '&to='+ to ;
+        
+        });
+        
+	    });
+	    }
+	  };
+	})(jQuery);
+
