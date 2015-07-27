d5534b9d618d097338eee3a6f3a7ce10b7934579
Nanda Kumar <nanda@globalsoftlab.com>
2015-07-27 19:37:48 +0530

    Social login

--- docroot/sites/all/modules/custom/autism_custom/autism_custom.js
+++ docroot/sites/all/modules/custom/autism_custom/autism_custom.js
@@ -6,18 +6,26 @@
      } 
     Drupal.behaviors.autism_custom = {
     attach: function (context, settings) {  
-      $.get(Drupal.settings.autism_custom.supporters_url, function( data ) { 
-        $('.member-count').attr('data-sp',data); 
-      });  
+     urlval = false;
+       if (Drupal.settings.autism_custom.supporters_url != 0) {
+        $.get(Drupal.settings.autism_custom.supporters_url, function( data ) {
+          if (isNaN(data) == false){ 
+            $('.member-count').attr('data-sp',data); 
+             urlval = true;
+          }
+        }); 
+      }  
       $(window).load(function () {
-        var spdata = parseInt($('.member-count').data('sp'));  
+        if(urlval == true){
+          var spdata = parseInt($('.member-count').data('sp'));
+        }  
         var spcount = parseInt(Drupal.settings.autism_custom.supporter_count);
         var valuetype = Drupal.settings.autism_custom.supporters_value_type;
         
-        if(valuetype == 'web'){
+        if(valuetype == 'web' && urlval == true){
           $("span.numbers").html(ReplaceNumberWithCommas(spdata));
         }
-        if(valuetype == 'both'){
+        if(valuetype == 'both' && urlval == true){
           var nval = spdata + spcount;
           $("span.numbers").html(ReplaceNumberWithCommas(nval));
         }
