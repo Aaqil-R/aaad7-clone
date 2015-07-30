6509e8b52b8ba413cd423a40166f0f940b2cccd3
Amalan Premkumar <amalanpremkumar@gmail.com>
2015-07-30 18:19:42 +0530

    changes in the get url from bsd.

---
+++ docroot/sites/all/modules/custom/autism_custom/test.php
@@ -0,0 +1,13 @@
+<?php
+	class test{
+		private $x = 100;
+
+		//new codes
+		private $url = '/utils/cons_counter/cons_counter.ajax.php';
+		//end lines of the new codes.
+
+		public function getx(){
+			return $this->x;
+		}
+	}
+?>
