01208dea56ca4efa8695bd2ae048755df61195db
Amalan Premkumar <amalanpremkumar@gmail.com>
2015-10-12 17:40:49 +0530

    webform share your story styles.

--- docroot/sites/all/themes/ambitious/templates/webform-form.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/webform-form.tpl.php
@@ -34,7 +34,8 @@
 
   // Print out the main part of the form.
   // Feel free to break this up and move the pieces within the array.
-  print drupal_render($form['submitted']);
+  
+  //print drupal_render($form['submitted']);
 
   // Always print out the entire $form. This renders the remaining pieces of the
   // form that haven't yet been rendered above (buttons, hidden elements, etc).
