1a66cca5538fe8189e4e4d5361d864744c60f3a4
Ayesha Munasinghe <Ayesha Munasinghe>
2016-04-01 12:23:45 +0530

    tpl file for policy topic page sub page

--- docroot/sites/all/themes/ambitious/templates/views-view-unformatted--stream--block-13.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-unformatted--stream--block-13.tpl.php
@@ -1,31 +1,24 @@
 <?php
+
 /**
  * @file
- * Default view template to display content in a Masonry layout.
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
  */
-// Variable
-if ($view->query->pager->total_items > $view->query->pager->options['items_per_page']){
-$no=(int) ($view->query->pager->total_items/$view->query->pager->options['items_per_page']);
-$noofpage = round($no,0, PHP_ROUND_HALF_DOWN);
-} else {
- $noofpage = 0;
-}
 ?>
+<!--<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>-->
 
 <?php
-  $additional_classes = "js-stream-intronew card-transparent";
-  if(isset($variables['node'])):
+	$additional_classes = "js-stream-intronew card-transparent";
+	if(isset($variables['node'])):
    $node = $variables['node'];
   endif;
 ?>
 
-<?php
-  $node = node_load(687506);
-      $content1 = $node->body['und'][0]['value'];
-      //print render($content1);
-?>
 <?php foreach ($rows as $id => $row): ?>
-  <?php //render($row); ?>
   <?php if ($id == 0 && isset($node)):?> 
     <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .' '. $additional_classes .'"';  } ?>>
       <!-- Teaser View of the topic --> 
@@ -35,24 +28,19 @@ $noofpage = round($no,0, PHP_ROUND_HALF_DOWN);
       <?php if(isset($node->body['und'][0]['safe_value'])): ?>
         <?php print $node->body['und'][0]['safe_value']; ?>
       <?php endif; ?>         
-      <!-- Teaser ends here --> 
+      <!-- Teaser ends here -->	
     </div>
-<?php endif; ?>
-    <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-      <?php print $row; ?>
-    </div>
-  
+  <?php else: ?>
+  <?php  
+    // Indicates a featured image.
+    $feature_indicator = "";
+    // Set the first three items as featured items.
+    if ($id < 4 && isset($node) ) {
+      $feature_indicator = "feature-row";
+    } 
+  ?>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] . " " . $feature_indicator .'"';  } ?>>
+    <?php print $row; ?>
+  </div>
+  <?php endif; ?>  
 <?php endforeach; ?>
-
-  <?php if($view->query->pager->current_page == $noofpage): ?>
-     <!-- <div class="<?php if ($classes_array[$id]) print ' ' . $classes_array[$id]; ?> navigation-block "> -->
-      <?php
-      //D7
-      //$block = module_invoke('block', 'block_view', '91');
-      //print render($block['content']);
-      // $node = node_load(687506);
-      // $content1 = $node->body['und'][0]['value'];
-      // print render($content1);
-      ?>
-    <!-- </div> -->
-  <?php endif; ?>
