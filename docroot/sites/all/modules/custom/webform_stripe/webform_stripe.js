2d53e04e82e607977c00341cd70960e4b822528e
Nishan Mills <nishan@thesmallaxe.com>
2016-04-01 16:34:58 +0530

    Accounting for charity place option in ambitious about autism event subscriptions. A bit of hard code, will revisit to make it more modular later.

--- docroot/sites/all/modules/custom/webform_stripe/webform_stripe.js
+++ docroot/sites/all/modules/custom/webform_stripe/webform_stripe.js
@@ -4,6 +4,7 @@
    * Ajax command to open Stripe Checkout, store result, and submit form.
    */
   Drupal.ajax.prototype.commands.stripeCheckout = function(ajax, data, status) {
+    console.log(data.params);
     // Open Stripe Checkout.
     StripeCheckout.open($.extend(data.params, {
       // Payment was successful.
@@ -15,6 +16,14 @@
         ajax.form[0].submit();
       }
     }));
+  };
+
+  /**
+   * Ajax command to proceed without a stripe checkout.
+   */
+  Drupal.ajax.prototype.commands.proceedWithoutCheckout = function(ajax, data, status) {
+    console.log(data.params);
+    ajax.form[0].submit();
   };
 
 }(jQuery));
