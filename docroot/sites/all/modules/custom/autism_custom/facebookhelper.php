ea7e7797dbfc9d2d1783723992651f371ab556d1
Amalan Premkumar <amalanpremkumar@gmail.com>
2016-08-19 15:51:54 +0530

    Adding try catch to function

--- docroot/sites/all/modules/custom/autism_custom/facebookhelper.php
+++ docroot/sites/all/modules/custom/autism_custom/facebookhelper.php
@@ -8,16 +8,24 @@
 		public $url = 'https://www.facebook.com/ambitiousaboutautism';
 
 		public function set_supporter_count(){
-			$this->fql  = "SELECT like_count";
-    		$this->fql .= " FROM link_stat WHERE url = '$this->url'";
- 
-    		$this->fqlURL = "https://api.facebook.com/method/fql.query?format=json&query=" . urlencode($this->fql);
- 
-    		// Facebook Response is in JSON
-    		$this->response = file_get_contents($this->fqlURL);
-    		//return json_decode($response);
-			$this->fb = json_decode($this->response , true);
-			return $fb_count = $this->fb[0]->like_count;
+			try{
+				$this->fql  = "SELECT like_count";
+	    		$this->fql .= " FROM link_stat WHERE url = '$this->url'";
+	 
+	    		$this->fqlURL = "https://api.facebook.com/method/fql.query?format=json&query=" . urlencode($this->fql);
+	 
+	    		// Facebook Response is in JSON
+	    		$this->response = file_get_contents($this->fqlURL);
+	    		//return json_decode($response);
+				$this->fb = json_decode($this->response , true);
+				return $fb_count = $this->fb[0]->like_count;
+			}
+			catch{
+				//If there are any errors return 0;
+				watchdog('autism_custom', 'Facebook count error');
+				return 0;
+			}
+			
 		}
 	}
 ?>
