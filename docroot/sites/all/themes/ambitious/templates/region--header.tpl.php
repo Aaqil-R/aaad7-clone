f29d141113c15316b87a7f3f8fd6919fce4a98b2
Ayesha Munasinghe <Ayesha Munasinghe>
2016-09-30 12:03:37 +0530

    change the donate button ridirection

--- docroot/sites/all/themes/ambitious/templates/region--header.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/region--header.tpl.php
@@ -38,7 +38,7 @@ global $user;
 		<div class="holder">
 				<div class="logo">
 					<a href="<?php print $link; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Ambitious About Autism'); ?>"></a>
-					<a class="btn donate-btn show-mobile" href="/donations/donation-page">Donate</a>
+					<a class="btn donate-btn show-mobile" href="/giving">Donate</a>
 				</div> <!-- /logo -->
 			<ul class="right-info">
 				<li><a href="#" class="school-opener"><span class="icon-Close"></span> <span class="menu-text">Our Schools &amp; College</span><span class="close">Close</span></a></li>
