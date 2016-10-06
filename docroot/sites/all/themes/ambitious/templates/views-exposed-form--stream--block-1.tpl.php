2df408f85d87bb14f8f4b71acc7b5f96592af471
Ayesha Munasinghe <Ayesha Munasinghe>
2016-10-06 14:27:15 +0530

    change the block view

--- docroot/sites/all/themes/ambitious/templates/views-exposed-form--stream--block-1.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-exposed-form--stream--block-1.tpl.php
@@ -31,20 +31,10 @@
 
 
 
-
-<div id="toggle-point"></div>
-
-<div class="sort-block">  
-  <div class="title-filter">
-    <?php print $widgets['filter-field_job_category_tid']->widget; unset($widgets['filter-field_job_category_tid']) ?>
-    
-  </div>
-  <div class="search-text job-category forum-page">
-      <?php print $widgets['search_box']->widget; unset($widgets['search_box']) ?>
-      <?php //print $widgets['filter-title']->widget; unset($widgets['filter-title']) ?>
-        <span class="icon-search"></span>
-  </div>
-				 <?php foreach ($widgets as $id => $widget): ?>
+<div class="sort-block">
+<div class="search-text forum-page"><input type="text" value="Search the community"></div>
+<?php //dpr($widgets); ?>
+  <?php foreach ($widgets as $id => $widget): ?>
       <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
         <?php if (!empty($widget->label)): ?>
           <label for="<?php print $widget->id; ?>">
@@ -55,21 +45,25 @@
           <div class="views-operator">
             <?php print $widget->operator; ?>
           </div>
-        <?php endif; ?>
-         
+        <?php endif; ?> 
+        <?php if (!empty($widget->widget)): ?>
+          <div class="views-widget">
+            <?php print $widget->widget; ?>
+          </div> 
+        <?php endif; ?> 
         <?php if (!empty($widget->description)): ?>
           <div class="description">
             <?php print $widget->description; ?>
           </div>
         <?php endif; ?>
-
       </div>
     <?php endforeach; ?>
     <?php if (!empty($sort_by)): ?>
       <div class="views-exposed-widget views-widget-sort-by">
         <?php print $sort_by; ?>
       </div>
-      <div class="views-exposed-widget views-widget-sort-order">
+      <?php //print $widget->widget; ?> 
+      <div class="views-exposed-widget views-widget-sort-order element-invisible">
         <?php print $sort_order; ?>
       </div>
     <?php endif; ?>
@@ -83,29 +77,14 @@
         <?php print $offset; ?>
       </div>
     <?php endif; ?>
-    
-    <div class="filter-overlay"></div>
-    
-    <div class="filter-slide">
-				<h4 class="title">Filter by..  <span class="filterbutton icon-Close"></span></h4>
-				<div class="nav-filter ">
-					 <?php print $widget->widget; ?> 
-				</div> 
-   
-      <div class="button-holder">
-       <div class="views-exposed-widget views-submit-button">
+     <div class="views-exposed-widget views-submit-button element-invisible">
       <?php print $button; ?>
-      <span class="btn clear-all">Clear all</span>
     </div>
-       </div> 
-				
-        </div>
     <?php if (!empty($reset_button)): ?>
-      <div class="views-exposed-widget views-reset-button element-invisible">
+      <div class="views-exposed-widget views-reset-button">
         <?php print $reset_button; ?>
       </div>
     <?php endif; ?> 
-    <a href="javascript:void()" class="btn btn-left forum-page topic filterbutton" title="Filter by topic"><span>Filter by topic <em class="icon-Plus"></em></span></a>
-    <?php print render($variables['share_button']); ?>
+
 </div>
  
