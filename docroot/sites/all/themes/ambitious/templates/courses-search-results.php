7165fd5e482739dcbe06459843fb54b35b31cce0
Amalan Premkumar <amalanpremkumar@gmail.com>
2016-10-12 16:00:25 +0530

    Final update to test the codes.

--- docroot/sites/all/themes/ambitious/templates/courses-search-results.php
+++ docroot/sites/all/themes/ambitious/templates/courses-search-results.php
@@ -1,2 +1,77 @@
-<h1>courses-search-results.php</h1>
-<?php print $snippet; ?>
+<?php 
+  $close_date_unix = date_timestamp_get(date_create($t->field_event_date['und'][0]['value2']));
+
+  // $close_date_unix = 1476867885;
+  //initializing the salary/location/commitment
+
+  $location = "";
+
+  // initialize closed date caption
+  $closed_date_caption = "Closed for applications";
+  $closed_date_class = "date-display-single closed";
+
+  // check if close date has passed
+  // if closed date has passed then 
+  // show text "Closed for applications"
+  // in red.
+  // if not closed the render as date.
+  // also checks whether the other fields 
+  // are popluated and prints them.
+  if (time() < $close_date_unix) {
+      $closed_date_caption = 'Closing Date: ' . date("Y-m-d",$close_date_unix);
+      $closed_date_class = "date-display-single";
+
+       if (isset($t->field_location[und][0][value])) {
+        $location = 'Location: ' . $t->field_location[und][0][value];
+      } 
+      else {
+        $location = 'Location: ' . $commitment;
+      }
+
+  }
+?>
+<div class="post card card--item" style="">
+  <section class="course-card">
+      <div> 
+        <a href="<?php print $url; ?>">
+          <?php
+
+            $viewitemsimage = array();
+
+            if(isset($t->field_featured_image)){
+              $getitemsimage = field_get_items('node', $t ,'field_featured_image');
+              $viewitemsimage = field_view_value('node'
+                , $t 
+                ,'field_featured_image'
+                , $getitemsimage[0]
+                , array('settings' => array('image_style' => 'tile_image')));
+
+              if(!isset($viewitemsimage['#item']))
+                  {  
+                   //rendering the default image
+                   $viewitemsimage = field_view_field('node', $t, 'field_featured_image', array('settings' => array('image_style' => 'tile_image')));
+                  }
+
+              print render($viewitemsimage);
+            } 
+
+          ?>
+        </a>
+      </div>
+      <div class="content">
+        <h3 <?php print $title_attributes; ?>>
+          <a href="<?php print $url; ?>"><?php print $title; ?></a>
+        </h3>
+        <p><?php print $close_date_unix; ?></p>
+        <div class="close-date">
+            <span class="<?php print $closed_date_class?>" property="dc:date" datatype="xsd:dateTime" content="2015-11-12T00:00:00+00:00"><?php print $closed_date_caption; ?></span>
+        </div>
+        <div class="salary">
+            <span class="<?php print $closed_date_class?>" ><?php print $salary; ?></span>
+        </div>
+        <div class="job-location">
+            <span class="<?php print $closed_date_class?>" ><?php print $location; ?></span>
+        </div>
+      </div>
+  </section>
+</div>
