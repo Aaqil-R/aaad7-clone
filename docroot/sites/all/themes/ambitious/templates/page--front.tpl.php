ece02f84301b6b1093ed11ff48aecf57071aa75d
Ayesha Munasinghe <Ayesha Munasinghe>
2017-03-21 13:18:15 +0530

    action block

--- docroot/sites/all/themes/ambitious/templates/page--front.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/page--front.tpl.php
@@ -232,6 +232,7 @@
         <div class="articles-columns" >
           <?php print render($page['services']); ?>
           <!--column one -->
+          <?php print views_embed_view('block_stream', 'block_stream_homepage_bottom');  ?>
         </div>
       </div>    
   </section><!-- /services -->
