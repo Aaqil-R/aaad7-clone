f6947cb465ad6e48f429f80a97ca3b9828f2ce88
Ayesha Munasinghe <Ayesha Munasinghe>
2017-03-24 12:14:46 +0530

    styling for call to action buttons

--- docroot/sites/all/themes/ambitious/templates/views-view-unformatted--call-to-action-buttons.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-unformatted--call-to-action-buttons.tpl.php
@@ -7,7 +7,34 @@
  * @ingroup views_templates
  */ 
  $myvar = get_defined_vars();  
-  // dpm($myvar);
+// dpm($myvar);
+ 
+$btn_color = ' ';
+
+ function pickColor($background_color) {
+  
+   switch ($background_color) {
+          case "#0075C7":
+             $btn_color = 'btn_navy_blue';
+              break;
+
+          case "#5BC5ED":
+             $btn_color = 'btn_light_blue';
+              break;
+
+          case "#00A837":
+             $btn_color = 'btn-green';
+              break;
+
+          case "#FBB900":
+             $btn_color = 'btn_yellow';
+              break;
+              
+          default:
+            $btn_color = "btn_orange";
+      }
+  return $btn_color;
+ }
 ?>
 
 
@@ -16,25 +43,21 @@
     <?php 
       $node_type = $myvar['variables']['view']->result[$id]->node_type;  
 
-
-      if(isset($myvar['variables']['view']->result[$id]->field_field_button_color[0]))
+    if(isset($myvar['variables']['view']->result[$id]->field_field_button_color[0]))
         $background_color = $myvar['variables']['view']->result[$id]->field_field_button_color[0]['rendered']['#markup'];
-      else 
-        $background_color = '';
 
-    	if(isset($myvar['variables']['view']->result[$id]->field_field_button_links[0]))
+
+      if(isset($myvar['variables']['view']->result[$id]->field_field_button_links[0]))
         $link = $myvar['variables']['view']->result[$id]->field_field_button_links[0]['raw']['url'];
 
-    if(isset($myvar['variables']['view']->result[$id]->field_field_button_links[0]))
+
+      if(isset($myvar['variables']['view']->result[$id]->field_field_button_links[0]))
         $title = $myvar['variables']['view']->result[$id]->field_field_button_links[0]['raw']['title'];
 
-      if(isset($myvar['variables']['view']->result[$id]->field_field_shadow_color[0]))
-        $shadow_color = $myvar['variables']['view']->result[$id]->field_field_shadow_color[0]['rendered']['#markup'];
-      else 
-        $shadow_color = '';
      ?>
 
-<div style="background:<?php print $background_color?>; box-shadow:0px 5px <?php print $shadow_color?>; !important;" class="btn cta-button"> <a href="<?php print $link ?>"> <?php print $title ?></a></div>
+<div class="btn btn_default <?php print pickColor($background_color); ?>"> <a href="<?php print $link ?>"> <?php print $title ?></a></div>
+<?php  $background_color = ' '; ?>
 
 <?php endforeach; ?>
  
