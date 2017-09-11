f923536eaadc48032b8b801c487c9ba38e608069
Ashhad <ashhads@thesmallaxe.com>
2017-09-11 12:22:19 +0530

    restricting to views page result

--- docroot/sites/all/themes/ambitious/templates/views-view-masonry--article-page.tpl.php
+++ docroot/sites/all/themes/ambitious/templates/views-view-masonry--article-page.tpl.php
@@ -7,36 +7,42 @@
 
 <?php 
 	$node = node_load(arg(1)); 
+	$count = count($rows);
 	if(isset($node->field_related_cards['und'])){
 		$count = count($rows) - count($node->field_related_cards['und']);
 	}
+	$view_object = views_get_view("stream");
+    $view_object->set_display("article_page");
+    $view_count = $view_object->display['article_page']->display_options['pager']['options']['items_per_page'];
 ?>
 
 <!-- Nodes Related articles -->
 <?php foreach ($node->field_related_cards['und'] as $id => $row): ?>
 	<?php 
 		$url = 'node/'.(string)$row['node']->nid; 
-		$img_url = file_create_url($row['node']->field_featured_image['und'][0]['uri'])
+		$img_url = file_create_url($row['node']->field_featured_image['und'][0]['uri']);
 	?>
-	<div class="masonry-item views-row post">
-		<div class="views-field views-field-nothing">
-			<span class="field-content">
-				<div class="img-holder">
-					<span class="icon-Playbutton video-icon"></span>
-					<a href="#" class="feature-holder">
-						<span class="icon-Featured"></span>
-						<span class="text">Featured </span>
-					</a>
-					<a href="<?php print drupal_get_path_alias($url); ?>"><img typeof="foaf:Image" src="<?php print $img_url; ?>" alt=""></a>
-				</div>
-				<div class="info add">
-					<h3>
-						<a href="<?php print drupal_get_path_alias($url); ?>"><?php print $row['node']->title  ?></a>
-					</h3>
-				</div>
-			</span>
+	<?php if($id < $view_count): ?>
+		<div class="masonry-item views-row <?php print $id ?> post">
+			<div class="views-field views-field-nothing">
+				<span class="field-content">
+					<div class="img-holder">
+						<span class="icon-Playbutton video-icon"></span>
+						<a href="#" class="feature-holder">
+							<span class="icon-Featured"></span>
+							<span class="text">Featured </span>
+						</a>
+						<a href="<?php print drupal_get_path_alias($url); ?>"><img typeof="foaf:Image" src="<?php print $img_url; ?>" alt=""></a>
+					</div>
+					<div class="info add">
+						<h3>
+							<a href="<?php print drupal_get_path_alias($url); ?>"><?php print $row['node']->title  ?></a>
+						</h3>
+					</div>
+				</span>
+			</div>
 		</div>
-	</div>
+	<?php endif; ?>
 <?php endforeach; ?>
 
 
